e370e555693e14694eb9c2deb581dd672ecea7d4
Marcel Rico <personal.mr4@gmail.com>
2020-03-01 15:44:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,7 +53,9 @@ class Student
       SELECT * FROM students WHERE name = ?
     SQL
     
-    DB[:conn].execute(sql,name)
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def update
